3482569f78f8aecb208e62b8c266adbc027e0e2b
Ceonzy <calilaraujo5@gmail.com>
2025-05-05 16:36:01 -0400

    Add files via upload

---
+++ def_par_impar.py
@@ -0,0 +1,12 @@
+def verificador_par_impar():
+    get_num = float(input("Insira um número para verificar se é par ou impar: "))
+    num = get_num
+
+    if num %2 == 0:
+        print(f"{num} é par.")
+
+    else:
+        print(f"{num} é impar.")
+    return 
+
+verificador_par_impar()
