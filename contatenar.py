b0bf1acf2d50fa98772e4a24479a6f2e79baeb29
Ceonzy <calilaraujo5@gmail.com>
2025-05-06 16:46:29 -0400

    Add files via upload

---
+++ contatenar.py
@@ -0,0 +1,9 @@
+numeros1 = (1, 2, 3, 2, 6, 2, 4, 1, 2)
+numeros2 = (4, 5, 6)
+
+todos_os_numero = numeros1 + numeros2
+
+print(f"Resultado das Tuplas contatenadas: {todos_os_numero}")
+
+contar = numeros1.count(2)
+print(f"O número 2 é repetido: {contar} vezes")
